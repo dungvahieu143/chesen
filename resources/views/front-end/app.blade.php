8788768aeba2751946d667e626bd0655d9c9e9c3
Minh Tiến Lê <minhtienlomo@gmail.com>
2022-04-18 22:02:26 +0700

    fix slider

--- resources/views/front-end/app.blade.php
+++ resources/views/front-end/app.blade.php
@@ -20,7 +20,7 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Comforter&family=Lora&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="//code.jquery.com/ui/1.13.0/themes/base/jquery-ui.css">
-    <script src="https://cdn.jsdelivr.net/npm/@splidejs/splide@latest/dist/js/splide.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@splidejs/splide@2.4.21/dist/js/splide.min.js"></script>
     <script src="{{ asset('js/myScript.js') }}"></script>
     <script src="https://kit.fontawesome.com/732f1c5837.js" crossorigin="anonymous"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
